a3f9bc8ae3cb95a00d0c9f0aa751a7cd80e7aa88
Hobin Yoon <hobinyoon@gmail.com>
2013-06-04 02:02:46 -0400

    do not manage Version.java

--- src/main/java/tachyon/Version.java
+++
@@ -1,8 +0,0 @@
-package tachyon;
-
-/**
- * The version of the current build. This class is auto generated in each compile.
- */
-public class Version {
-  public static final String VERSION = "0.2.1-SNAPSHOT";
-}
