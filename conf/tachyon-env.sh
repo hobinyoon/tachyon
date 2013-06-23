8f5487b515e517aba8a2c1dc6432d18a059717f1
Hobin Yoon <hobinyoon@gmail.com>
2013-06-22 20:01:02 -0700

    Local FS as underlying FS.

--- conf/tachyon-env.sh
+++ conf/tachyon-env.sh
@@ -2,16 +2,20 @@
 
 CONF_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-export TACHYON_HDFS_ADDRESS=hdfs://localhost:9000
+# HDFS
+#export TACHYON_UNDERFS_ADDRESS=hdfs://localhost:9000
+
+# Local
+export TACHYON_UNDERFS_ADDRESS=/home/hobin/work/tachyon
 
 export TACHYON_JAVA_OPTS="
   -Dtachyon.debug=true
   -Dtachyon.worker.timeout.ms=60000
-  -Dtachyon.underfs.address=$TACHYON_HDFS_ADDRESS
+  -Dtachyon.underfs.address=$TACHYON_UNDERFS_ADDRESS
   -Dtachyon.worker.memory.size=1024MB
   -Dtachyon.worker.data.folder=/mnt/ramdisk/tachyonworker/
   -Dtachyon.master.hostname=localhost
-  -Dtachyon.master.log.file=$TACHYON_HDFS_ADDRESS/tachyon/tachyon_log.data
-  -Dtachyon.master.checkpoint.file=$TACHYON_HDFS_ADDRESS/tachyon/tachyon_checkpoint.data
+  -Dtachyon.master.log.file=$TACHYON_UNDERFS_ADDRESS/tachyon/tachyon_log.data
+  -Dtachyon.master.checkpoint.file=$TACHYON_UNDERFS_ADDRESS/tachyon/tachyon_checkpoint.data
   -Dtachyon.master.pinlist=/pinfiles;/pindata
 "
